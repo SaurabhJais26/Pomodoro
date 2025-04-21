638b602ac2548bcded9e62a1223cd4bb9ba76473
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-04-21 23:09:53 +0530

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Saurabh Jaiswal on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
